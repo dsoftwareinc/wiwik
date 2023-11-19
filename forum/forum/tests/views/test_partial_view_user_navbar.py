1a18ee95c566f3baa860e401f9facfa588885df5
Daniel M <daniel.maruani@gmail.com>
2023-11-19 17:01:09 -0500

    fix:test for bookmarks

---
+++ forum/forum/tests/views/test_partial_view_user_navbar.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+from django.urls import reverse
+
+from forum.models import QuestionBookmark
+from forum.tests.base import ForumApiTestCase
+from forum.views import utils
+
+
+class TestUserNavbarPartialView(ForumApiTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.question = utils.create_question(cls.users[0], cls.title, cls.question_content, ','.join(cls.tags))
+        cls.answer = utils.create_answer(cls.answer_content, cls.users[1], cls.question, False)
+        QuestionBookmark.objects.create(user=cls.users[1], question=cls.question)
+
+    def test_user_navbar_partial_view__should_show_bookmarks__when_user_has(self):
+        self.client.login(self.usernames[1], self.password)
+        res = self.client.user_navbar()
+        self.assertEqual(200, res.status_code)
+        soup = BeautifulSoup(res.content, 'html.parser')
+
+        bookmark_url = reverse('forum:thread', kwargs={'pk': self.question.pk})
+        self.assertEqual(1, len(soup.select(f'a[href="{bookmark_url}"]')))
+
+    def test_user_navbar_partial_view__should_show_no_bookmarks__when_user_has_none(self):
+        self.client.login(self.usernames[0], self.password)
+        res = self.client.user_navbar()
+        self.assertEqual(200, res.status_code)
+
+        self.assertContains(res, 'No bookmarks')
