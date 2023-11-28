52813009dc4edd470d93869a6c6a6fd2ae2ac540
Daniel M <daniel.maruani@gmail.com>
2023-11-28 17:26:44 -0500

    refactor:improve code

--- forum/forum/views/thread_views/view_invite_to_question.py
+++ forum/forum/views/thread_views/view_invite_to_question.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -13,6 +13,8 @@ from userauth.models import ForumUser
 
 @login_required
 def view_users_autocomplete(request):
+    if request.method != 'GET':
+        raise Http404()
     qs = ForumUser.objects.all()
     query = request.GET.get('q', None)
     selected = request.GET.get('selected', None)
@@ -31,22 +33,22 @@ def view_users_autocomplete(request):
 
 @login_required
 def view_invite_to_question(request, question_pk: int):
-    if request.method == "POST":
-        inviter = request.user
-        invitee_usernames = request.POST.dict()['usernames']
-        invitee_usernames = invitee_usernames.split(',')
-        question = Question.objects.filter(pk=question_pk).first()
-        if question is None:
-            logger.warning(f'{inviter.username} is trying to invite users "{invitee_usernames}" '
-                           f'to question {question_pk} which does not exist')
-            return redirect(reverse('forum:thread', args=[question_pk]))
-        invitees = (ForumUser.objects
-                    .filter(username__in=invitee_usernames)
-                    .exclude(username=question.author.username))
-        count = utils.create_invites_and_notify_invite_users_to_question(inviter, invitees, question)
-        logger.debug(f"Inviting users to question succeeded: {inviter.username}:invited {invitee_usernames}")
-        if count > 0:
-            messages.success(request, "Successfully invited user(s) to this question.")
+    if request.method != "POST":
+        logger.warning("Someone is trying to make a request not through the app")
+        return redirect('forum:thread', pk=question_pk)
+    inviter = request.user
+    invitee_usernames = request.POST.dict()['usernames']
+    invitee_usernames = invitee_usernames.split(',')
+    question = Question.objects.filter(pk=question_pk).first()
+    if question is None:
+        logger.warning(f'{inviter.username} is trying to invite users "{invitee_usernames}" '
+                       f'to question {question_pk} which does not exist')
         return redirect(reverse('forum:thread', args=[question_pk]))
-    logger.warning("Someone is trying to make a request not through the app")
-    return redirect('forum:thread', pk=question_pk)
+    invitees = (ForumUser.objects
+                .filter(username__in=invitee_usernames)
+                .exclude(username=question.author.username))
+    count = utils.create_invites_and_notify_invite_users_to_question(inviter, invitees, question)
+    logger.debug(f"Inviting users to question succeeded: {inviter.username}:invited {invitee_usernames}")
+    if count > 0:
+        messages.success(request, "Successfully invited user(s) to this question.")
+    return redirect(reverse('forum:thread', args=[question_pk]))
